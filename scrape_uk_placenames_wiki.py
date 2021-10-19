4c683484419c7d3a782ea7ad73d4f77039878ef5
dapontesmith <65298051+dapontesmith@users.noreply.github.com>
2021-10-19 14:43:51 -0400

    getting coordinates

--- scrape_uk_placenames_wiki.py
+++ scrape_uk_placenames_wiki.py
@@ -25,7 +25,7 @@ for a in soup.find_all('a', href=True):
 #set base url
 base_url = "https://en.wikipedia.org"
 
-
+coord_holder, longitude, latitude = [], [], []
 df = pd.DataFrame()
 #iterate over linsk to get all place names 
 for link in links:
@@ -33,22 +33,37 @@ for link in links:
     #make full url
     full_url = base_url + link
     html = requests.get(full_url).content
-    #read html
+    #get the coordinates out 
+    
+    #read html to get place names 
     df_list = pd.read_html(html)
     #get rid of extraoenous first and last elements of df_list 
-    df_list = df_list[1:len(df_list)]
+    #and concat all dfs together
+    df_list = pd.concat(df_list[1:len(df_list)])
+    #df = df_list.iloc[ : , 0:2 ]
     df = df.append(df_list)
-    
+
 #eliminate header rows  
 df.columns = ["location","locality","coordinates","grid_reference"]
 df = df[df["location"].str.contains("Location")==False]
-df = df[["location","locality","coordinates"]]
+df_clean = df.dropna()
+#df = df[["location","locality"]]
 
 #coordinates are not in the right form, but i have all the place_names
-df_names = df[["location","locality"]]
+df_names = df_clean[["location","locality"]]
 
+#
+coord_list = []
+coordinates = df[["coordinates"]].values.tolist()
+#get list of coordinates
+for i in range(len(coordinates)):
+    print(i)
+    coord_list.append(coordinates[i][0].replace(u"\ufeff","").split(" / ")[0])
+
+df_names["coordinates"] = coord_list
+
+#write to csv
 df_names.to_csv("C:/Users/dapon/Dropbox/Harvard/dissertation/data/uk_geography/uk_placenames_wiki.csv")
-
 
 
 
@@ -59,10 +74,37 @@ soup = BeautifulSoup(res.content,'lxml')
 tables = soup.find_all('table',{'class':'wikitable'})
 table = tables[0].find('tbody')
 
+holder, longitude, latitude = [], [], []
+for i in range(len(tables)):
+    table = tables[i].find("tbody")
+    coords = table.find("span",{"class":"geo"}).text
+    holder.append(coords)
+longitude = [holder[i].split("; ")[0] for i in range(len(holder))]
+latitude = [holder[i].split("; ")[1] for i in range(len(holder))]
+
+
+
+
 for row in table.find_all('tr',{'class':'geo-default'}):
     coords = (row.find('span',{'class': 'geo-dec'}))
     
     
     
     
+    soup = BeautifulSoup(html)
+    tables = soup.find_all("table",{"class":"wikitable"})
+   # for i in range(len(tables)):
+    #    table = tables[i].find("tbody")
+     #   coords = table.find("span",{"class":"geo"}).text
+      #  coord_holder.append(coords)
     
+latitude = [coord_holder[i].split("; ")[0] for i in range(len(coord_holder))]
+longitude = [coord_holder[i].split("; ")[1] for i in range(len(coord_holder))]  
+
+
+
+
+
+for i in range(len(df)):
+    test[i] = df.iloc[i, 2].replace(u"\ufeff","").split(" / ")[0]
+test = test[1]
